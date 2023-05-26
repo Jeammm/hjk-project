84f3e5f3addadd66194bf85c6ebe8314acd78b2d
Jeammm <athicha.phjkl@gmail.com>
2023-05-26 19:28:28 +0700

    seperate server and app function

--- HJK-webservice/backend/index.js
+++ HJK-webservice/backend/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const compression = require("compression");
 const cors = require("cors");
-const app = express();
-const port = 8000;
+const cookieParser = require("cookie-parser");
+
+const AppError = require("./utils/AppError");
+const errorController = require("./controllers/errorController");
 const productRouter = require("./routes/productRouter");
 const userRouter = require("./routes/userRouter");
 
-const xss = require('xss-clean');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const compression = require('compression');
-
+const app = express();
 
 //set security for http headers
 app.use(helmet());
@@ -17,18 +19,23 @@ app.use(helmet());
 const limiter = rateLimit({
   max: 100,
   wondowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour',
+  message: "Too many requests from this IP, please try again in an hour",
 });
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 //body parser, reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 
 //data sanitiztion against XSS
 app.use(xss());
 
 //enable cors
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(
   express.urlencoded({
@@ -38,38 +45,16 @@ app.use(
 
 app.use(compression());
 
-app.get("/", (req, res) => {
-  res.json({ message: "ok" });
-});
+app.use(cookieParser());
 
+//routes
 app.use("/api/v1", productRouter);
 app.use("/api/v1/user", userRouter);
 
-/* Error handler middleware */
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  console.error(err.message, err.stack);
-  res.status(statusCode).json({ message: err.message });
-
-  return;
+//error middleware routes
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+app.use(errorController);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-process.on('unhandledRejection', (err) => {
-  console.error(err.name, err.message, '💥');
-  console.error('Shutting down...');
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on('uncaughtException', (err) => {
-  console.error(err.name, err.message, '💥');
-  console.error('Shutting down...');
-  server.close(() => {
-    process.exit(1);
-  });
-});
+module.exports = app;
