29a39f0dbef6c29dafa6e7d9d79f7d38319f6926
Jeammm <athicha.phjkl@gmail.com>
2023-05-26 19:27:52 +0700

    add authentication and logout api

--- HJK-webservice/backend/routes/userRouter.js
+++ HJK-webservice/backend/routes/userRouter.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.post('/signup', authController.signup)
 router.post('/login', authController.login)
+router.post('/logout', authController.logout)
+router.post('/isAuthorized', authController.isAuthorized)
 
 
 module.exports = router;
